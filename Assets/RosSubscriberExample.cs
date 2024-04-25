130a1110d03fb88d3c4f7c80e5e700b2e6d2f2ed
karim-y <mhy16@mail.aub.edu>
2024-04-25 15:15:53 +0200

    Commented subscriber script

--- Assets/RosSubscriberExample.cs
+++ Assets/RosSubscriberExample.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
-//using RosColor = RosMessageTypes.UnityRoboticsDemo.UnityColorMsg;
-using OGGM = RosMessageTypes.Nav.OccupancyGridMsg;
-using Posemsg = RosMessageTypes.Geometry.PoseMsg;
 using pc2m = RosMessageTypes.Sensor.PointCloud2Msg;
 using twist = RosMessageTypes.Geometry.TwistMsg;
 using transformer = RosMessageTypes.CustomInterfaces;
-using Geo = RosMessageTypes.Geometry;
+
 using Nav = RosMessageTypes.Nav;
 using StringMsg = RosMessageTypes.Std.StringMsg;
 using TMPro;
@@ -20,30 +17,13 @@ public class RosSubscriberExample : MonoBehaviour
 {
     //public GameObject cube;
     public RosPublisherExample pub;
-    [NonSerialized]
-    public sbyte[] arr;
-    [NonSerialized]
-    public byte[] pcarr;
-
     public GameObject[] ButtonsForMaps;
-
-    [NonSerialized]
-    public float resol;
-    [NonSerialized]
-    public float heig;
-    [NonSerialized]
-    public float wid;
-
     public GameObject objectToEnable;
-    private Posemsg odom;
     [NonSerialized]
     public uint pcwidth;
     [NonSerialized]
-    public pc2m incomingPointCloudDownSampled, localPointCloudDownSampled, incomingPointCloudLive;
+    public pc2m incomingPointCloudLive;
     
-    //public Geo.PoseStampedMsg GoalPose;
-    //public Nav.PathMsg Path;
-    //Nav.OdometryMsg Odometry;
     [NonSerialized]
     public double x, y, z, rx, ry, rz;
     public MiniMapIncoming mmincom;
@@ -53,98 +33,57 @@ public class RosSubscriberExample : MonoBehaviour
     GameObject PathParent, PathElement;
     Vector3 Shift = new Vector3();
     Vector3 FixedShift = new Vector3(0, 0, 0);
-    Vector3 PathGoal = new Vector3();
-    List<string> ReceivedMapNames = new List<string>();
+    //Vector3 PathGoal = new Vector3();
+
+    Vector3 posePath = new();
+    Vector3 posePath2 = new();
+    Vector3 dir = new();
+    Quaternion quaternion = new();
+    Vector3 rotationAngles = new();
+    Vector3 translation = new();
+
+    List<string> SavedNamesList = new List<string>();
+
     void Start()
     {
 
-        //ROSConnection.GetOrCreateInstance().Subscribe<RosColor>("color", ColorChange);
-        ROSConnection.GetOrCreateInstance().Subscribe<OGGM>("/mapToUnity", Ocupo);
-        ROSConnection.GetOrCreateInstance().Subscribe<pc2m>("/robot_map_downsampled", pointCloud); // No need for them anymore
-        ROSConnection.GetOrCreateInstance().Subscribe<pc2m>("/local_map_downsampled", localPointCloud);
-        ROSConnection.GetOrCreateInstance().Subscribe<pc2m>("/com/semantic_pcl", pointCloudLive);
-        ROSConnection.GetOrCreateInstance().Subscribe<transformer.TransformationMsg>("/refined_tf", twistReceived); //This should become the edited message type that has idto idfrom
-        //ROSConnection.GetOrCreateInstance().Subscribe<twist>("/trans_topic_merger", twistReceived);
-        ROSConnection.GetOrCreateInstance().Subscribe<pc2m>("/com/downsampled", pointCloudDownsampled);
-        ROSConnection.GetOrCreateInstance().Subscribe<pc2m>("/human/human_label", pointCloudDownsampledTest);
-
-        ROSConnection.GetOrCreateInstance().Subscribe<Nav.PathMsg>("/plan", PathDataSub);
-        ROSConnection.GetOrCreateInstance().Subscribe<Nav.OdometryMsg>("/odom", OdomSub);
-
-        ROSConnection.GetOrCreateInstance().Subscribe<StringMsg>("/com/map_names", MapNames);
-
-        //new
-        //ROSConnection.GetOrCreateInstance().Subscribe<OGGM>("occupancy_map", Ocupo);
+        ROSConnection.GetOrCreateInstance().Subscribe<pc2m>("/com/semantic_pcl", ReceiveMergedMap);
+        ROSConnection.GetOrCreateInstance().Subscribe<transformer.TransformationMsg>("/refined_tf", ReceiveTwist);
+        ROSConnection.GetOrCreateInstance().Subscribe<pc2m>("/com/downsampled", ReceiveDownsampledMaps);
+        ROSConnection.GetOrCreateInstance().Subscribe<Nav.PathMsg>("/plan", ReceiveRobotPath);
+        ROSConnection.GetOrCreateInstance().Subscribe<StringMsg>("/com/map_names", ReceiveSavedMapNames);
 
     }
-    public void Ocupo(OGGM bata)
+    
+    public void ReceiveMergedMap(pc2m MergedPointCloud)
     {
-        resol = bata.info.resolution;
-        heig = bata.info.height;
-        wid = bata.info.width;
-        arr = bata.data;
-        objectToEnable.SetActive(true);
-        //cube.GetComponent<Renderer>().material.color = new Color32((byte)colorMessage.r, (byte)colorMessage.g, (byte)colorMessage.b, (byte)colorMessage.a);
+        incomingPointCloudLive = MergedPointCloud;
+        Debug.Log("Map received");   
     }
-    public void pointCloud(pc2m ptcld)
-    {
-        incomingPointCloudDownSampled = ptcld;
-    }
-    public void pointCloudLive(pc2m ptcldlive)
-    {
-        incomingPointCloudLive = ptcldlive;
-        Debug.Log("Ejit");
-        //mvd.ShowMergedMap();
-        //Debug.Log(ptcldlive.data[17]);    
-    }
-    public void twistReceived(transformer.TransformationMsg Twisty)
+
+    public void ReceiveTwist(transformer.TransformationMsg TransformMsg)
     {
 
-
-        /*x = 1 * Twisty.linear.y;
-        y = -1 * Twisty.linear.z;
-        z = -1 * Twisty.linear.x;
-        rx = 1 * Twisty.angular.y * Mathf.Rad2Deg;
-        ry = -1 * Twisty.angular.z * Mathf.Rad2Deg;
-        rz = -1 * Twisty.angular.x * Mathf.Rad2Deg;*/
-
-        /*x = -1 * Twisty.linear.y;
-        y = -1 * Twisty.linear.z;
-        z = 1 * Twisty.linear.x;
-        rx = 1 * Twisty.angular.y * Mathf.Rad2Deg;
-        ry = 1 * Twisty.angular.z * Mathf.Rad2Deg;
-        rz = 1 * Twisty.angular.x * Mathf.Rad2Deg; //was -1
-        */
-        /*Debug.Log(Twisty.tf.linear.x);
-        Debug.Log(Twisty.tf.linear.y);
-        Debug.Log(Twisty.tf.linear.z);
-        Debug.Log(Twisty.tf.angular.x);
-        Debug.Log(Twisty.tf.angular.y);
-        Debug.Log(Twisty.tf.angular.z);*/
-        if (Twisty.idfrom == 2)
+        if (TransformMsg.idfrom == 2)
         {
-            x = -1 * Twisty.tf.linear.x;
-            y = -1 * Twisty.tf.linear.z;
-            z = -1 * Twisty.tf.linear.y;
-            rx = 1 * Twisty.tf.angular.x * Mathf.Rad2Deg;
-            ry = 1 * Twisty.tf.angular.z * Mathf.Rad2Deg;
-            rz = 1 * Twisty.tf.angular.y * Mathf.Rad2Deg; //was -1
-            //Debug.Log(ry);
+            x = -1 * TransformMsg.tf.linear.x;
+            y = -1 * TransformMsg.tf.linear.z;
+            z = -1 * TransformMsg.tf.linear.y;
+            rx = TransformMsg.tf.angular.x * Mathf.Rad2Deg;
+            ry = TransformMsg.tf.angular.z * Mathf.Rad2Deg;
+            rz = TransformMsg.tf.angular.y * Mathf.Rad2Deg;
+
+            rotationAngles.Set((float)rx, (float)ry, (float)rz);
+            translation.Set((float)x, (float)y, (float)z);
+
             pub.FirstAlignment = false;
         }
-        
-        
-
     }
 
-    public void localPointCloud(pc2m localptcld)
+    public void ReceiveDownsampledMaps(pc2m downsampled)
     {
-        localPointCloudDownSampled = localptcld;
-        //Debug.Log("Stla2ayna");
-    }
+        // The downsampled maps are for aligning purposes
 
-    public void pointCloudDownsampled(pc2m downsampled)
-    {
         if (downsampled.header.stamp.nanosec <= 1)
         {
             pub.IDto = (int)downsampled.header.stamp.nanosec;
@@ -161,36 +100,11 @@ public class RosSubscriberExample : MonoBehaviour
         }
 
     }
-    public void pointCloudDownsampledTest(pc2m label)
+
+    public void ReceiveRobotPath(Nav.PathMsg path)
     {
-
-        //Debug.Log(label.data.Length);
-        //Debug.Log(label.data[13]+" "+label.data[14]);
-        /*for (int i = 0; i < 32; i++)
-        {
-            Debug.Log(i + ": " + label.data[i]);
-        }*/
-
-    }
-
-    public void PathDataSub(Nav.PathMsg path)
-    {
-        Debug.Log("eja pathhh");
-        Vector3 posePath = new Vector3();
-        Vector3 posePath2 = new Vector3();
-        Vector3 dir = new Vector3();
-        Quaternion quaternion = new Quaternion();
-        //Debug.Log(path.poses[path.poses.Length  - 1].pose.position);
-        // Check if we got a new goal from Rviz
-        posePath.Set((float)path.poses[path.poses.Length - 1].pose.position.x, (float)path.poses[path.poses.Length - 1].pose.position.z, (float)path.poses[path.poses.Length - 1].pose.position.y);
-
-
-        if (PathGoal != posePath)
-        {
-            PathGoal = posePath;
-            //FixedShift = Shift;
-        }
-
+        // If the node is publishing numerous times for the same path, then it might be better to implement a "check for change" condition. But that is for when we are receiving real time odom updates from the moving robot.
+        Debug.Log("Path received");
 
         //Delete old path points
         if (PathParent != null) Destroy(PathParent);
@@ -198,55 +112,35 @@ public class RosSubscriberExample : MonoBehaviour
 
         //Instantiate new path objects (arrows)
         PathParent = new GameObject("PathParent");
-        PathParent.transform.Rotate(new Vector3((float)rx, (float)ry, (float)rz), Space.Self);
-        PathParent.transform.Translate(new Vector3((float)x, (float)y, (float)z), Space.Self);
+        PathParent.transform.Rotate(rotationAngles, Space.Self);
+        PathParent.transform.Translate(translation, Space.Self);
         for (int i = 0; i < path.poses.Length - 1; i++)
         {
+            // Get path point
             posePath.Set((float)path.poses[i].pose.position.x, (float)path.poses[i].pose.position.z, (float)path.poses[i].pose.position.y);
 
-            //posePath -= FixedShift;
-            //posePath = TransformFromGlobalToLocal(posePath);
-            
+            // Get consequent path point
             posePath2.Set((float)path.poses[i + 1].pose.position.x, (float)path.poses[i + 1].pose.position.z, (float)path.poses[i + 1].pose.position.y);
-            //posePath2 -= FixedShift;
-            //posePath2 = TransformFromGlobalToLocal(posePath2);
-
+            
+            // Get direction between two points to align the path arrow with it
             dir = posePath2 - posePath;
 
             quaternion.SetFromToRotation(Vector3.forward, dir);
 
-            PathElement = Instantiate(objectToEnable, posePath, quaternion);
-            PathElement.transform.SetParent(PathParent.transform, false);
+            PathElement = Instantiate(objectToEnable, posePath, quaternion, PathParent.transform);
 
         }
     }
-    public Vector3 TransformFromGlobalToLocal(Vector3 poiint)
+
+    public void ReceiveSavedMapNames(StringMsg NameMsg)
     {
-        Vector3 rotationAngles = new Vector3((float)rx, (float)ry, (float)rz);
-        Vector3 translation = new Vector3((float)x, (float)y, (float)z);
+        // Creates new button for newly added name in the list
         
-        Quaternion rotationQuaternion = Quaternion.Euler(rotationAngles);
-        
-        Vector3 point = rotationQuaternion * poiint + translation;
-
-        return point;
-    }
-    public void OdomSub(Nav.OdometryMsg odom)
-    {
-        //When Vuforia is able to localize the robot, please add the transform that vuforia returns to the shift.
-        // Also don't forget to account for the rotation of the robot
-
-        Shift.Set((float)odom.pose.pose.position.x, (float)odom.pose.pose.position.z, (float)odom.pose.pose.position.y);
-
-    }
-
-    public void MapNames(StringMsg msg)
-    {
-        if (!ReceivedMapNames.Contains(msg.data.ToLower()))
+        if (!SavedNamesList.Contains(NameMsg.data.ToLower()))
         {
-            ReceivedMapNames.Add(msg.data.ToLower());
-            ButtonsForMaps[ReceivedMapNames.Count - 1].SetActive(true);
-            ButtonsForMaps[ReceivedMapNames.Count - 1].GetComponent<ButtonConfigHelper>().MainLabelText = msg.data.ToLower();
+            SavedNamesList.Add(NameMsg.data.ToLower());
+            ButtonsForMaps[SavedNamesList.Count - 1].SetActive(true);
+            ButtonsForMaps[SavedNamesList.Count - 1].GetComponent<ButtonConfigHelper>().MainLabelText = NameMsg.data.ToLower();
         }
     }
 }
