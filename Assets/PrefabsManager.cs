eb2207d2ba868465e818af1b0a5e8ed860f13bbe
Adam Manasfi <manasfiadam4@gmail.com>
2024-11-29 09:37:46 +0200

    voxel size 0.1

--- Assets/PrefabsManager.cs
+++ Assets/PrefabsManager.cs
@@ -22,7 +22,7 @@ public class PrefabsManager : MonoBehaviour
 
     private void Start()
     {
-        VoxelSize = 0.05f;
+        VoxelSize = 0.1f;
         ChunkSize = 3f;
         voxelPrefab = VoxelPrefab;
         addedVoxelPrefab = AddedVoxelPrefab;
